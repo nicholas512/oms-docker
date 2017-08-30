a14cf0777053654f6d3140c146b4a3a9a69f923b
sidereus <francesco.serafin.3@gmail.com>
2017-08-30 11:51:57 -0600

    Added R binding support

--- beta/entrypoint.sh
+++ beta/entrypoint.sh
@@ -7,4 +7,14 @@ then
     ant all
 fi
 
+cd Rlibs/
+
+if [ ! -d build/ ];
+then
+    mkdir build/
+    R CMD INSTALL -l build/ source/*.tar.gz
+fi
+
+cd ..
+
 java -Doms3.work=/work -cp ".:/root/.oms/3.5.26/oms-all.jar:lib/*:dist/*" oms3.CLI -r $1

7a566e140e1b19b04abba5b4889cd72a6506621f
sidereus <francesco.serafin.3@gmail.com>
2017-08-30 18:05:25 -0600

    Added java flag -Xmx12288M to use more memory

--- beta/entrypoint.sh
+++ beta/entrypoint.sh
@@ -21,4 +21,4 @@ then
     cd ..
 fi
 
-java -Doms3.work=/work -cp ".:/root/.oms/3.5.26/oms-all.jar:lib/*:dist/*" oms3.CLI -r $1
+java -Xmx12288M -Doms3.work=/work -cp ".:/root/.oms/3.5.26/oms-all.jar:lib/*:dist/*" oms3.CLI -r $1

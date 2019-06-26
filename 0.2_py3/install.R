ae46b6bdc18f77f7df485723dce2af4b82b50ea0
sidereus <francesco.serafin.3@gmail.com>
2019-06-26 16:00:19 -0600

    new: v0.2

---
+++ 0.2_py3/install.R
@@ -0,0 +1,8 @@
+#!/usr/bin/env Rscript
+
+install.packages(c("pkgconfig","Matrix","magrittr",
+                   "irlba","R6","sys","askpass",
+                   "openssl","curl","mime","jsonlite",
+                   "igraph","XML","httr","miniCRAN",
+                   "Rserve","Rcpp"),
+repos="http://lib.stat.cmu.edu/R/CRAN")
